774da0f4e5a5f3cb51daf3349d03f42f521d1f15
bhavishyaravi0424-commits <bhavishyaravi0424@gmail.com>
2026-05-07 11:45:38 +0530

    Update app.py

--- calculator-backend/app.py
+++ calculator-backend/app.py
@@ -2,47 +2,43 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Home Route
 @app.route("/")
 def home():
-    return "Python Backend Connected to Railway Successfully!"
+    return "Python Backend Connected Successfully!"
 
-# Calculator API
 @app.route("/calculate", methods=["POST"])
 def calculate():
+
     data = request.get_json()
 
     num1 = data.get("num1")
     num2 = data.get("num2")
     operation = data.get("operation")
 
-    try:
-        if operation == "add":
-            result = num1 + num2
+    if operation == "add":
+        result = num1 + num2
 
-        elif operation == "subtract":
-            result = num1 - num2
+    elif operation == "subtract":
+        result = num1 - num2
 
-        elif operation == "multiply":
-            result = num1 * num2
+    elif operation == "multiply":
+        result = num1 * num2
 
-        elif operation == "divide":
-            if num2 == 0:
-                return jsonify({"error": "Cannot divide by zero"})
-            result = num1 / num2
+    elif operation == "divide":
+        if num2 == 0:
+            return jsonify({"error": "Cannot divide by zero"})
 
-        else:
-            return jsonify({"error": "Invalid operation"})
+        result = num1 / num2
 
-        return jsonify({
-            "num1": num1,
-            "num2": num2,
-            "operation": operation,
-            "result": result
-        })
+    else:
+        return jsonify({"error": "Invalid operation"})
 
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    return jsonify({
+        "num1": num1,
+        "num2": num2,
+        "operation": operation,
+        "result": result
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
